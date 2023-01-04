aec8878dd90d78a1bbf3b398bcb9783c3121c217
vinayakpagar23 <vinayakpagar1999@gmail.com>
2023-01-04 17:42:35 +0530

    navbar

--- flipkart/src/App.js
+++ flipkart/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Box } from '@mui/material'
 import Header from './components/LandingPage/Header/Header';
-
+import Home from './components/LandingPage/Home/Home'
 function App() {
   return (
     <>
    <Header/>
+   <Box style={{marginTop: 54}}>
+   <Home/>
+   </Box>
+ 
     </>
   );
 }
